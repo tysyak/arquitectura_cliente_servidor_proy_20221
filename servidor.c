632027b0ab1032785d3756d2d7e9134387c35e6a
Cristian Romero Andrade <mascrit@gmail.com>
2021-11-30 00:39:23 -0600

    Se crea servidor

---
+++ servidor.c
@@ -0,0 +1,46 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include "sservidor.h"
+
+int main()
+{
+
+	servidor * serv = servidor_new();
+
+	while(1)
+	{
+		bzero(serv->read_buffer, size);
+		bzero(serv->write_buffer, size);
+
+		printf("\nEsperando Cliente...\n");
+		serv->client_len=sizeof(serv->client_addrobj);
+		serv->client_fd = accept(serv->server_fd, (struct sockaddr *)&serv->client_addrobj, &serv->client_len);
+
+		if(serv->client_fd == -1)
+			perror("\e[31mError en la conexión\e[0m\n");
+		else
+			printf("\e[32mConexión exitosa\e[0m\n");
+
+		serv->ret = pthread_create(&serv->thread_id, NULL, thread_function, serv);
+		while(strncmp(serv->write_buffer, ":q", 3) != 0 )
+		{
+			strcpy(serv->write_buffer, "\0" );
+			printf("Servidor(tu)>");
+			fgets(serv->write_buffer, size, stdin);
+			serv->bytes = send(serv->client_fd, serv->write_buffer, sizeof(serv->write_buffer), 0);
+
+			if(serv->bytes == -1)
+				perror("\e[31mEl servidor fallo\e[0m\n");
+		}
+
+		pthread_join(serv->thread_id, &serv->retval);
+		printf("Hasta la próxima");
+		close(serv->client_fd);
+	}
+
+	close(serv->server_fd);
+    return 0;
+}
+
---
+++ sservidor.c
@@ -0,0 +1,54 @@
+#include "sservidor.h"
+
+servidor* servidor_new(){
+    int True = 1;
+
+    servidor* serv = malloc(sizeof(servidor));
+
+    unlink("SOCKET SERVER");
+
+    serv->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(serv->server_fd, SOL_SOCKET, SO_REUSEADDR, &True, sizeof(int));
+    if (serv->server_fd == -1)
+		perror("\e[31mCreación del socket fallo\e[0m\n");
+
+	else
+        printf("\e[32mExito en la creación del socket\e[0m\n");
+
+    serv->server_addrobj.sin_family = AF_INET;
+	serv->server_addrobj.sin_port = htons(PORT);
+	serv->server_addrobj.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	serv->ret = bind(serv->server_fd, (const struct sockaddr *)&serv->server_addrobj, sizeof(serv->server_addrobj));
+	if(serv->ret==-1)
+		perror("\e[31mBinding falló\e[0m\n");
+    else
+		printf("\e[32mExito en el Binding\e[0m\n");
+
+    serv->ret = listen(serv->server_fd, 5);
+	if(serv->ret==-1)
+		perror("\e[31mFallo creando el Listening\e[0m\n");
+	else
+		printf("\e[32mExito creando el Listening\e[0m\n");
+
+    return serv;
+}
+
+void * thread_function(void * arg)
+{
+	servidor *serv = arg;
+
+	bzero(serv->read_buffer, size);
+	while( strncmp(serv->read_buffer, ":q", 3) != 0 )
+	{
+		bzero(serv->read_buffer, size);
+		serv->bytes = recv(serv->client_fd, serv->read_buffer, size, 0);
+		if(serv->bytes == -1){
+            sleep(5);
+			perror("\e[31mFallo en la lectura del cliente\e[0m\n");
+		}else
+			printf("\nCliente> %s \nServidor(tu)>", serv->read_buffer);
+	}
+
+	pthread_exit(NULL);
+}
