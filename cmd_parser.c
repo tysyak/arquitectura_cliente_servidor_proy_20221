78e803109191f214d96cf60fc2eadef0878ec3e7
Cristian Romero <mascrit@gmail.com>
2021-12-03 22:49:33 -0600

    Implemento el comando `cd`

--- cmd_parser.c
+++ cmd_parser.c
@@ -19,6 +19,11 @@ void strip_chars(char *str, char strip)
 void  ejecutar(char *ejecutar, servidor * serv) {
   char *exe = ejecutar;
   cmd *comando = malloc(sizeof(cmd) * 3);
+  int itera = 0;
+  int itera_aux = 0;
+  char letter = ' ';
+  unsigned int tamanio_cadena;
+  int aux_flag = 0;
 
   FILE *fp;
   char path[size];
@@ -26,18 +31,40 @@ void  ejecutar(char *ejecutar, servidor * serv) {
   strip_chars(exe, '\n');
 
   strcpy(comando->completo_comando, exe);
+  tamanio_cadena = strlen(comando->completo_comando);
 
-  fp = popen(comando->completo_comando, "r");
-  if (fp == NULL) {
-    printf("Failed to run command\n");
-    exit(1);
+
+  while(itera <= tamanio_cadena) {
+    letter = comando->completo_comando[itera];
+    if (letter == ' ') {
+      aux_flag = 1;
+      itera++;
+      letter = comando->completo_comando[itera];
+    }
+    if (aux_flag == 0)
+      comando->comando[itera] = letter;
+    else {
+      comando->argumentos[itera_aux] = letter;
+      itera_aux++;
+    }
+    itera++;
   }
+  if (strcmp(comando->comando, "cd")==0) {
+      chdir(comando->argumentos);
+  } else {
+    fp = popen(comando->completo_comando, "r");
+    if (fp == NULL) {
+      printf("Failed to run command\n");
+      exit(1);
+    }
 
-  /* Read the output a line at a time - output it. */
-  while (fgets(path, sizeof(path), fp) != NULL) {
+    /* Read the output a line at a time - output it. */
+    while (fgets(path, sizeof(path), fp) != NULL) {
       serv->bytes = send(serv->client_fd, path, sizeof(path), 0);
+    }
+    pclose(fp);
   }
-  pclose(fp);
+
   printf("> ");
   /* close */
 
